c0737f481cd7f8a1d23e71a88445ce41668a02c8
Agrima KP <qvistn111@gmail.com>
2021-04-21 23:05:03 +0530

    formatted

--- src/oddoreven.cc
+++ src/oddoreven.cc
@@ -2,23 +2,28 @@
 
 using namespace std;
 
-bool oddOrEven(int n){
-  if(n%2 == 0){
+bool oddOrEven(int n)
+{
+  if (n % 2 == 0)
+  {
     return 0;
   }
   return 1;
 }
 
-int main(){
+int main()
+{
 
- int n;
- cout << "Enter a number \n";
- cin >> n;
+  int n;
+  cout << "Enter a number \n";
+  cin >> n;
 
- if(oddOrEven(n) == 0){
-   cout << "Even";
- } else {
-   cout << "Odd";
- }
-
+  if (oddOrEven(n) == 0)
+  {
+    cout << "Even";
+  }
+  else
+  {
+    cout << "Odd";
+  }
 }
