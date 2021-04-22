d962fe47135b23c2fc842ff95e050b1e2abf58e6
Agrima KP <qvistn111@gmail.com>
2021-04-22 22:46:01 +0530

    display equation of a line in the form ax + by = c, input a,b,c

---
+++ src/chapter-1/equation.cc
@@ -0,0 +1,15 @@
+// display equation of a line in the form ax + by = c, input a,b,c
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+  int a, b, c;
+  cout << "Enter values for a, b and c";
+  cin >> a >> b >> c;
+
+  cout << a <<"x + " << b << "y = " << c;
+}
