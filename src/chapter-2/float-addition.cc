a1f6a9d6e7ec0f71a163708812a1eebece27dd8b
Agrima KP <qvistn111@gmail.com>
2021-04-27 18:59:38 +0530

    add two floating point numbers

---
+++ src/chapter-2/float-addition.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   float m, n;
+
+   cout << "Enter 2 float type numbers : ";
+   cin >> n >> m;
+
+   float result = n/m;
+
+   cout << result;
+}
