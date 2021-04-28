b086292b241eab2a08e426f0123180b373b8e177
Agrima KP <qvistn111@gmail.com>
2021-04-28 22:02:58 +0530

    basic

---
+++ src/chapter-2/basic2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  float x, y;
+  int z;
+
+
+  x = 111.3;
+  y = 22.567;
+
+
+  z = x + y;
+
+
+  cout << x << endl;
+  cout << y << endl;
+  cout << z << endl;
+}
