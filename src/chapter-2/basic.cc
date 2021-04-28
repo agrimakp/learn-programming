12429328e61c26ac7476e33bb2287158d404bb38
Agrima KP <qvistn111@gmail.com>
2021-04-28 21:56:46 +0530

    basic

---
+++ src/chapter-2/basic.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+  //1Q
+
+  int x, y;
+  short int z;
+
+  //2Q
+
+  x = 111111;
+  y = 222222;
+
+  //3Q
+
+  z = x + y;
+
+  //4Q
+
+  cout << x << endl;
+  cout << y << endl;
+  cout << z << endl;
+  cout << SHRT_MAX << endl;
+}
