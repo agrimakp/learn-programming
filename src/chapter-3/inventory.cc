3dce0eefc721007ab06049cc17ce840052f3df2b
Agrima KP <qvistn111@gmail.com>
2021-05-07 12:47:21 +0530

    inventory

--- src/chapter-3/inventory.cc
+++ src/chapter-3/inventory.cc
@@ -6,3 +6,16 @@
 using namespace std;
 
 int main()
+{
+  float demand_rate, setup_cost, holdng_cost;
+  cout << "Input values Demand Rate (d), Setup cost (s), Holding cost (h)";
+  cin >> demand_rate >> setup_cost >> holdng_cost;
+
+  float eoq = sqrt((2 * demand_rate * setup_cost) / holdng_cost);
+
+  cout << "EOQ = " << eoq << endl;
+
+  float tbo = sqrt((2 * setup_cost) / (demand_rate * holdng_cost));
+
+  cout << "TBO = " << tbo << endl;
+}
