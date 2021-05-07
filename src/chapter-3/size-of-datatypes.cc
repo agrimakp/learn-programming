79577801a4b1577cd58a041ad6664e32d9a0c469
Agrima KP <qvistn111@gmail.com>
2021-05-07 13:29:11 +0530

    size of data types

---
+++ src/chapter-3/size-of-datatypes.cc
@@ -0,0 +1,36 @@
+// 11
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+
+  cout << "short int = " << sizeof(short int) << endl;
+  cout << "int = " << sizeof(int) << endl;
+  cout << "long int = " << sizeof(long) << endl;
+  cout << "------------------------------------------" << endl;
+
+  cout << "double = " << sizeof(double) << endl;
+  cout << "long double = " << sizeof(long double) << endl;
+
+  cout << "------------------------------------------" << endl;
+
+  cout << "char = " << sizeof(char) << endl;
+  cout << "float = " << sizeof(float) << endl;
+  cout << "string = " << sizeof(string) << endl;
+  cout << "bool = " << sizeof(bool) << endl;
+
+  cout << "------------------------------------------" << endl;
+
+  cout << "signed int = " << sizeof(signed int) << endl;
+  cout << "unsigned int = " << sizeof(unsigned int) << endl;
+
+  cout << "signed char = " << sizeof(signed char) << endl;
+  cout << "unsigned char = " << sizeof(unsigned char) << endl;
+
+
+}
