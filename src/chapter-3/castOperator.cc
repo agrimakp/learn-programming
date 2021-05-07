3b616b2d251c4ff0f1d59da03198bbd24ff93fc9
Agrima KP <qvistn111@gmail.com>
2021-05-07 18:00:32 +0530

    .

---
+++ src/chapter-3/castOperator.cc
@@ -0,0 +1,19 @@
+// 17 cast is a special operator that forces one data type to be converted into another
+
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+
+int main()
+{
+  double a = 23.45648;
+  int c = int(a);
+
+  cout << "int (a) = " << c;
+
+}
