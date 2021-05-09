3602414df6308f98d4950bf4f90703bb40620774
Agrima KP <qvistn111@gmail.com>
2021-05-09 15:50:43 +0530

    3

---
+++ src/chapter-4/horizondal-bar-chart.cc
@@ -0,0 +1,35 @@
+//56
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void printBar(int input)
+{
+  for (int i = 0; i < 1; i++)
+  {
+    for (int j = 0; j < input; j++)
+    {
+      cout << " * ";
+    }
+    cout << input;
+    cout << "\n";
+  }
+}
+
+int main()
+{
+  float n, m, l, k;
+  cout << "enter a row limit";
+  cin >> n >> m >> l >> k;
+
+  float inputn = round(n); //2
+  float inputm = round(m); //3
+  float inputl = round(l);
+  float inputk = round(k);
+
+  printBar(inputn);
+  printBar(inputm);
+  printBar(inputl);
+  printBar(inputk);
+}
