1c30330357cc20ec634aaa6474c56bbbfab8723d
Agrima KP <qvistn111@gmail.com>
2021-05-11 17:01:09 +0530

    .

---
+++ src/chapter-5/floyds-triangle.cc
@@ -0,0 +1,40 @@
+//62
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int number = 1;
+  cout << "(a)" << endl;
+  for (int i = 1; i <= 13; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      cout << number << " ";
+      number += 1;
+    }
+    cout << "\n";
+  }
+
+
+  cout << "(2)" << endl;
+  int num = 0;
+  for (int i = 1; i <= 13; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      if (num % 2 == 0)
+      {
+        cout << "1 ";
+      }
+      else
+      {
+        cout << "0 ";
+      }
+
+      num += 1;
+    }
+    cout << "\n";
+  }
+}
