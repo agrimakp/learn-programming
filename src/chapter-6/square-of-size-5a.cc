d4f4b70847604baec5f6c057e8ab9c49e3d4320b
Agrima kp <qvistn111@gmail.com>
2021-05-15 20:33:00 +0530

    .

---
+++ src/chapter-6/square-of-size-5a.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+  for (int i = 1; i <= 5; i++)
+  {
+    for (int j = 1; j <= 5; j++)
+    {
+
+      if(j == i && j == 3){
+        cout <<  " O ";
+      } else {
+        cout << " S ";
+      }
+    }
+    cout << "\n";
+  }
+}
