8c6c80be3aeac481cc7bcfa94c5898eb4a4757f2
Agrima <qvistn111@gmail.com>
2022-02-01 19:23:35 +0530

    compute the sum of the squares of  number[]

--- javaScript/summOfSquares.js
+++ javaScript/summOfSquares.js
@@ -1,1 +1,15 @@
-console.log("hai");
+const number = [1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9,10.10];
+console.log("entered "+ number.length + " real numbers")
+for (let index = 0; index < number.length; index++) {
+  const element = number[index];
+  console.log(element);
+}
+// compute the sum of thier squares
+let total = 0.0;
+for (let index = 0; index < number.length; index++) {
+  total = total + number[index] * number[index];
+}
+
+console.log(total.toFixed(2))
+
+
